32f6c6eb2b0f64a7ad6ca464a5604c5ecb42817f
AllanContreras <allan.contreras-r@mail.escuelaing.edu.co>
2025-10-28 21:14:44 -0500

    Cleanup: remove unused Link import in App.jsx; rebuild ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom'
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Lobby from './pages/Lobby'
