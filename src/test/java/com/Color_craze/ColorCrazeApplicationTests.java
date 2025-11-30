ef9d7dc207f251629b180e1efc5d312fbf3c7440
AllanContreras <allan.contreras-r@mail.escuelaing.edu.co>
2025-11-30 18:57:21 -0500

    test: add contextLoads() to execute tests and produce JaCoCo report

--- src/test/java/com/Color_craze/ColorCrazeApplicationTests.java
+++ src/test/java/com/Color_craze/ColorCrazeApplicationTests.java
@@ -1,9 +1,13 @@
 package com.Color_craze;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 class ColorCrazeApplicationTests {
-
+	@Test
+	void contextLoads() {
+		// Verifies Spring context starts without exceptions
+	}
 
 }
